2a51dcb9df50ba6253d6e2ff76212e9b19f40278
Juan Montoya <juanmontoya@Juans-MBP.attlocal.net>
2024-02-11 20:18:01 -0600

    before migrations

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -4,9 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function createPost() {
-    
+
 }
 
 function loadPosts() {
-
+    fetch('posts/main', {
+        method: "GET"
+    })
+        .then(r => r.json())
+        .then(result => {
+            for (let count = 0; count < result.length; count++) {
+                console.log(result[count])
+            }
+        })
 }
